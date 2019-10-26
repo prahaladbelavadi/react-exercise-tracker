4b19b4cfb71e47e2397b7dc00a8c043ae5ab7a83
Prahalad Belavadi <prahalad@belavadi.com>
2019-10-26 19:27:03 +0530

    bug when rendering

---
+++ src/components/create-exercise.component.js
@@ -0,0 +1,14 @@
+import { React, Component } from "react";
+
+export default class CreateExercises extends Component {
+    render(){
+        return (
+            <div>
+                <p>
+                    You're in the create ExercisesList Component
+                </p>
+            </div>
+        )
+        
+    }
+}
